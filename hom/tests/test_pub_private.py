b5dd44d1d0f16749e9107039485dc0bd69d0b793
Romeo <rbullajr@terra.com.br>
2024-06-20 15:31:24 +0000

    Versão de testes com BUs reais.

--- hom/tests/test_pub_private.py
+++ hom/tests/test_pub_private.py
@@ -1,18 +1,27 @@
 import requests
+import glob
 from auxiliary import processar_response
 
-def test_bu_create():
+def bu_create(bu_name:str, folder:str):
   url = "http://127.0.0.1:9090/bu/create"
 
   payload = {}
   files=[
-    ('file',('o00407-0101500020065.bu',open(file='o00407-0101500020065.bu',mode='rb'),'application/octet-stream'))
+    ('file',(bu_name,open(file=f'{folder}/{bu_name}',mode='rb'),'application/octet-stream'))
   ]
   headers = {}
 
   response = requests.request("POST", url, headers=headers, data=payload, files=files)
   texto, result = processar_response(response)
   assert result.status == "ok"
+
+
+def test_bu_create():
+  folder:str = 'bus'
+  bu_array = glob.glob("*.bu", root_dir=folder)
+
+  for bu in bu_array:
+    bu_create(bu, folder)
 
 
 def test_commit_all_trees():
@@ -24,3 +33,6 @@ def test_commit_all_trees():
   response = requests.request("POST", url, headers=headers, data=payload)
   texto, result = processar_response(response)
   assert result.commited_trees
+
+
+test_bu_create()
